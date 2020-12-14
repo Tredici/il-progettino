8c2513b7336c617ee588a76d39176b6a342cdd70
Tredici <marcolucio1999@gmail.com>
2020-12-14 21:32:17 +0100

    Basic rb_tree interface defined.
    Ho definito le prime funzioni ("metodi") che servono a inizializzare e distruggere un albero rosso nero.

---
+++ rb_tree.h
@@ -0,0 +1,18 @@
+/** Espone una classe "albero rosso-nero"
+ * Efficiente ed utile, da usare dove serve.
+ */
+#ifndef RB_TREE
+#define RB_TREE
+
+#include <stdlib.h>
+#include <string.h>
+
+struct rb_tree;
+
+struct rb_tree* rb_tree_init(struct rb_tree*);
+struct rb_tree* rb_tree_clear(struct rb_tree*);
+void rb_tree_destroy(struct rb_tree*);
+
+
+
+#endif
