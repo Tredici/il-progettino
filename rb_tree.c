d324d19759a7b439ce3550efb93c3e57cb0cc001
Tredici <marcolucio1999@gmail.com>
2020-12-16 16:51:18 +0100

    Implemented rb_tree_set.
    Implementato il "metodo" per inserire un nuovo valore (o aggiornare un precedente) in un albero rosso nero data una chiave.

--- rb_tree.c
+++ rb_tree.c
@@ -128,5 +128,156 @@ static elem* left_rotate(struct rb_tree* tree, elem* node)
 }
 
 
+/** Ripristina le proprietà di un albero rosso nero
+ *  in seguito a un inserimento
+ * Cormen pag. 261
+ */
+static void rb_tree_insert_fixup(struct rb_tree* tree, elem* new_item)
+{
+/* Ottiene il colore del nodo in questione */
+#define GET_COLOR(n)  ((n) != NULL ? (n)->col    : BLACK)
+/* Ottiene il padre, eventualmente NULL */
+#define GET_PARENT(n) ((n) != NULL ? (n)->parent : NULL )
+/* Ottiene il colore del padre, default NERO */
+#define GET_PCOLOR(n) ((n)->parent != NULL ? (n)->parent->col    : BLACK)
+/* È un figlio destro o sinistro */
+#define IS_LEFT_SON(n) ((n)->parent->left == (n))
+#define IS_RIGHT_SON(n) ((n)->parent->right == (n))
+
+    elem* parent;
+    elem* uncle;
+    elem* grandpa;
+
+    /* ripeti fino a che non ripristini la situazione */
+    while (GET_PCOLOR(new_item) == RED)
+    {
+        parent  = GET_PARENT(new_item);
+        grandpa = GET_PARENT(parent);
+
+        if (IS_LEFT_SON(parent))
+        {
+            /* il padre è un figlio sinistro */
+            uncle = grandpa->right; /* quindi lo zio è un figlio destro */
+
+            if (GET_COLOR(uncle) == RED) /* Padre e zio rossi */
+            {
+                /* "Sposta" il problema verso la radice */
+                parent->col = BLACK;
+                uncle->col = BLACK;
+                grandpa->col = RED;
+                /*A questo punto il n. di nodi neri in ciascun percorso è invariato*/
+                new_item = grandpa;
+            }
+            else /* Lo zio è nero */
+            {
+                if (IS_RIGHT_SON(parent)) /* Caso 2: figlio destro di un sinistro */
+                {
+                    new_item = parent;
+                    left_rotate(tree, new_item);
+                }
+                /* Caso 3, eseguito SEMPRE dopo il 2 */
+                parent->col  = BLACK;
+                grandpa->col = RED;
+                right_rotate(tree, grandpa);
+            }
+        }
+        else
+        {
+            /* Speculare a sopra, invertendo destra e sinistra */
+            /* il padre è un figlio destro */
+            uncle = grandpa->left;
+
+            if (GET_COLOR(uncle) == RED)
+            {
+                parent->col = BLACK;
+                uncle->col = BLACK;
+                grandpa->col = RED;
+                new_item = grandpa;
+            }
+            else
+            {
+                if (IS_LEFT_SON(parent))
+                {
+                    new_item = parent;
+                    right_rotate(tree, new_item);
+                }
+                parent->col  = BLACK;
+                grandpa->col = RED;
+                left_rotate(tree, grandpa);
+            }
+        }
+    }
+    /* Colora la radice di nero */
+    tree->root->col = BLACK;
+
+/* Elimina la macro create apposta */
+#undef IS_RIGHT_SON
+#undef IS_LEFT_SON
+#undef GET_PCOLOR
+#undef GET_PARENT
+#undef GET_COLOR
+}
+
+
+/** Inserisce un nuovo nodo nell'albero o aggiorna il valore corrente
+ *
+ * Ritorna 0 in caso di successo, -1 in caso di errore.
+ */
+int rb_tree_set(struct rb_tree* tree, long int key, void* val)
+{
+    elem* new_item;
+    elem* curr, *next;
+
+    curr = NULL;
+    next = tree->root;
+
+    while (next != NULL)
+    {
+        curr = next;
+
+        if (curr->key < key)
+        {
+            /* i maggiori vanno a destra */
+            next = curr->right;
+        }
+        else if (curr->key > key)
+        {
+            /* i minori a sinistra */
+            next = curr->left;
+        }
+        else
+        {
+            /* CASO SPECIALE! Aggiorna vecchio valore! Non fa altro */
+            curr->value = val;
+            return 0;
+        }
+    }
+    /* Ha trovato il punto dell'albero dove finirà */
+    new_item = elem_init(key, val);
+    if (new_item == NULL)
+    {
+        return -1;
+    }
+    new_item->parent = curr;
+    if (curr == NULL)
+    {
+        /* l'albero è vuoto, aggiungiamo la radice */
+        tree->root = new_item;
+    }
+    else if (curr->key < key)
+    {
+        /* maggiore: posizionato a destra */
+        curr->right = new_item;
+    }
+    else
+    {
+        /* minore: posizionato a sinistra */
+        curr->left = new_item;
+    }
+    /* Potrebbe aver infranto la regola dei colori */
+    rb_tree_insert_fixup(tree, new_item);
+
+    return 0;
+}
 
 
--- rb_tree.h
+++ rb_tree.h
@@ -13,6 +13,7 @@ struct rb_tree* rb_tree_init(struct rb_tree*);
 struct rb_tree* rb_tree_clear(struct rb_tree*);
 void rb_tree_destroy(struct rb_tree*);
 
+int rb_tree_set(struct rb_tree*, long int, void*);
 
 
 #endif
