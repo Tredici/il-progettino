2e7d890e876f35dbcd917c0d14c13b1954d3384f
Tredici <marcolucio1999@gmail.com>
2020-12-20 20:37:30 +0100

    Iniziato sviluppo sistema multi-io.
    Ho iniziato lo svilupo di una raccolta di funzioni per permettere a più thread di accedere alle funzionalità di io (in particolare di output) insieme senza sovrapporsi causando disastri.

---
+++ multi_io.h
@@ -0,0 +1,14 @@
+/**
+ * Raccolta di funzioni necessarie per permettere 
+ * a più thread di eseguire operazioni di io insieme
+ * risolvendo tutti i problemi di condivisione del 
+ * canale di io
+ */
+
+
+#ifndef MULTI_IO
+#define MULTI_IO
+
+#endif
+
+
