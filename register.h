e36ad4076beeeeb752cdb38ea450833c9d13db83
Tredici <marcolucio1999@gmail.com>
2020-12-29 13:39:27 +0100

    Defined textual representention of a entry.
    Ho definito quale sarà la rappresentazione testuale di una entry.

--- register.h
+++ register.h
@@ -8,5 +8,17 @@
 
 struct entry;
 
+/** La struttura testuale di un entry
+ * sarà:
+ *  \d{4}-\d{2}-\d{2}|[NT]|\d+
+ *
+ * Una pipe '|' farà da separatore
+ * e i tre campi saranno rispettivamente:
+ *  +data
+ *  +tipologia
+ *  +numero di persone coinvolte
+ *
+ */
+
 
 #endif
