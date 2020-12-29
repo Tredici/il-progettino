18411bc3d6840d0e508d7976dba4933f278a12a7
Tredici <marcolucio1999@gmail.com>
2020-12-29 02:40:07 +0100

    Defined entry structure.
    Ho definito come saranno tenute in memoria le entry.

--- register.c
+++ register.c
@@ -1,4 +1,24 @@
 #include "register.h"
+#include <time.h>
 
+enum entry_type
+{
+    SWAB,       /* tampone */
+    NEW_CASE    /* nuovo caso */
+};
 
+/** Una entry necessiterà di:
+ *  tempo:
+ *      +data
+ *      ? +ora
+ *  tipo:
+ *      >tampone
+ *
+ */
 
+struct entry
+{
+    struct tm e_time;
+    enum entry_type type;
+    int counter;
+};
