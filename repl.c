627b5001ca87ef70c511b530b7296802f690438f
Tredici <marcolucio1999@gmail.com>
2020-12-30 13:18:13 +0100

    Implemented repl_recognise_cmd.
    Definita un funzione per eseguire facilmente il parsing di comandi ottenuti da un ciclo repl.

--- repl.c
+++ repl.c
@@ -1,2 +1,35 @@
 #include "repl.h"
+#include <string.h>
 
+struct repl_cmd repl_recognise_cmd(const char* text,
+    const struct repl_cmd_hint cmds[])
+{
+    size_t cmdLen;
+    struct repl_cmd ans;
+    const struct repl_cmd_hint* cmd;
+    const char* args;
+
+    ans.command = text;
+    ans.flag = -1;
+
+    for (cmd = &cmds[0]; cmd != NULL; ++cmd)
+    {
+        cmdLen = strlen(cmd->command);
+
+        /* Test del comando */
+        if (strncasecmp(text, cmd->command, cmdLen) == 0)
+        {
+            ans.flag = cmd->flag;
+            
+            for (args = &text[cmdLen]; *args != '\0' && 0 == isgraph(*args); ++args)
+                ;
+            
+            ans.args = args;
+            break;
+        }
+
+        ++cmds;
+    }
+
+    return ans;
+}
--- repl.h
+++ repl.h
@@ -7,5 +7,52 @@
 #ifndef REPL
 #define REPL
 
+/** Massimo numero di caratteri
+ * ('\0' terminale escluso) per
+ * identificare un comando
+ */
+#define REPL_CMD_MAX_L 20
+
+/** Permette di riconoscere un comando
+ * e specificare un flag per identificare
+ * il comando riconosciuto
+ */
+struct repl_cmd_hint
+{
+    char command[REPL_CMD_MAX_L+1];
+    int flag;
+};
+
+/** Raccoglie le informazioni su un comando
+ * riconosciuto:
+ *  +puntatore alla stringa analizzata
+ *  +puntatore al primo argomento che segue
+ *      il comando, o a '\0' se non ve sono
+ *  +flag che permette di riconoscere il 
+ *      comando facilmente, o -1 se questo
+ *      non è stato riconosciuto
+ */
+struct repl_cmd
+{
+    const char* command;
+    const char* args;
+    int flag;
+};
+
+/** Riconosce il comando se tra quelli indicati
+ * e restituisce una struttura che permette di
+ * identificare il comando in questione
+ *
+ * Il primo argomento è la stringa in cui cercare
+ * un comando, il secondo è un array di
+ * repl_cmd_hint terminato da 
+ * (struct repl_cmd_hint*) NULL che specifica i
+ * comandi da individuare
+ *
+ * Il controllo del comando è CASE-INSENSITIVE
+ */
+struct repl_cmd repl_recognise_cmd(const char*, const struct repl_cmd_hint[]);
+
+
 
 #endif
