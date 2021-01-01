4cf11010997d1f109694636288f3793d0e8dc456
Tredici <marcolucio1999@gmail.com>
2021-01-01 23:29:22 +0100

    Started develop of peer.
    Ho iniziato lo sviluppo del codice dei peer.
    La cartella peer sarà usata per non confondere i file di utilizzo esclusivo dei peer da quelli di "utility" eventualmente utilizzabili anche nello sviluppo del server o in generale in altri progetti in quanto volutamente il più generali possibile.

---
+++ peer.c
@@ -0,0 +1,13 @@
+/** File che conterrà il sorgente dei peer
+ * Il codice sarà testato man mano PRIMA
+ * di essere unito in un prodotto
+ * definitivo
+ */
+
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+
