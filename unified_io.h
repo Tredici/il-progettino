d129ae80e6b41638f64a89ed1fe411bf421a9f1b
Tredici <marcolucio1999@gmail.com>
2021-01-05 21:33:38 +0100

    Started dev unified.IO.
    Ho iniziato lo sviluppo delle due funzioni per passare gli output dai thread secondari a quello principale.

---
+++ unified_io.h
@@ -0,0 +1,18 @@
+/** Funzioni per fare in modo di trasferire
+ * l'IO dai thread secondari a quello
+ * principale
+ *
+ * In pratica quello che si fa è usare una
+ * coda di messaggi per passare i messaggi
+ * da stampare dai thread secondari a quello
+ * dedicato all'IO
+ */
+
+#ifndef UNIFIED_IO
+#define UNIFIED_IO
+
+
+
+
+
+#endif
