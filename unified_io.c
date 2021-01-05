6e252436502dc53cd1f79bf0e73d5223b81ee009
Tredici <marcolucio1999@gmail.com>
2021-01-06 00:14:25 +0100

    Defined struct io_message and implemented io_message_init.
    Definito il tipo che sarà usato per contenere le informazioni sui messaggi e la funzione che sarà utilizzata per crearli.

--- unified_io.c
+++ unified_io.c
@@ -4,6 +4,31 @@
 #include <string.h>
 #include <stdlib.h>
 
+/** Struttura che sarà usata per conservare
+ * i messaggi generati dai vari thread.
+ */
+struct io_message
+{
+    enum unified_io_type type;
+    char* msg;
+};
+
+struct io_message* io_message_init(const char* msg, enum unified_io_type type)
+{
+    struct io_message* ans;
+
+    ans = malloc(sizeof(struct io_message));
+    if (ans == NULL)
+        return NULL;
+
+    /* più per tradizione che per necessità */
+    memset(ans, 0, sizeof(struct io_message));
+    ans->type = type;
+    ans->msg = strdup(msg);
+
+    return ans;
+}
+
 /** Coda per i messaggi che genereranno
  * i vari thread.
  */
