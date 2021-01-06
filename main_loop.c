5fe3743e6390befdb52b72e97fc5c03889f544f4
Tredici <marcolucio1999@gmail.com>
2021-01-06 19:59:28 +0100

    Implemented help command handler.
    Ho definito la funzione che sarà utilizzata per gestire il comando di help.

--- main_loop.c
+++ main_loop.c
@@ -7,3 +7,29 @@
 #include <termios.h>
 #include <unistd.h>
 
+/* variabili utili per l'help */
+static int commN;
+static const struct main_loop_command* commV;
+
+/** Funzione corrispondente
+ * al comando di help.
+ *
+ * Come è ragionevole aspettarsi stampa
+ * una semplice descrizione di tutti i
+ * comandi forniti.
+ */
+static int help(const char* args)
+{
+    int i;
+    (void)args;
+
+    /* descrizione di help stesso */
+    printf("\t%s:\n\t\t%s\n", "help", "stampa nome e descrizione dei comandi disponibili");
+    /* ciclo per la descrizione degli
+     * altri comandi */
+    for (i = 0; i != commN; ++i)
+        printf("\t%s:\n\t\t%s\n", commV[i].command, commV[i].description);
+
+    return OK_CONTINUE;
+}
+
