5caa38b2b59a2785c29d3bc8d27ddb1baf266bf1
Tredici <marcolucio1999@gmail.com>
2021-01-06 20:06:26 +0100

    Implemented makeRCTODOV.
    Implementata la funzione per convertire l'input di main_loop nel formato richiesto da repl_start.

--- main_loop.c
+++ main_loop.c
@@ -33,3 +33,34 @@ static int help(const char* args)
     return OK_CONTINUE;
 }
 
+/** Genera l'array di struct repl_cmd_todo
+ * a partire da quello di
+ *  struct main_loop_command*
+ */
+static struct repl_cmd_todo*
+makeRCTODOV(const struct main_loop_command* commands,
+            int len)
+{
+    struct repl_cmd_todo* ans;
+    int i;
+
+    if (commands == NULL || len < 0)
+        return NULL;
+
+    /* il +1 è per lasciare spazio al comando di help */
+    ans = calloc(sizeof(struct repl_cmd_todo), len+1);
+    if (ans == NULL)
+        return NULL;
+
+    for (i = 0; i < len; ++i)
+    {
+        ans[i].fun = commands[i].fun;
+        strncpy(ans[i].command, commands[i].command, REPL_CMD_MAX_L+1);
+    }
+    /* inserisce il comandi di help */
+    ans[i].fun = &help;
+    strncpy(ans[i].command, "help", REPL_CMD_MAX_L+1);
+
+    return ans;
+}
+
