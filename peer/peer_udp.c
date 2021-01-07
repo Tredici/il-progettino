466d06d5ba91c3425a130f911d23a2fe44b2e52c
Tredici <marcolucio1999@gmail.com>
2021-01-07 14:36:48 +0100

    Implemented first version of UDP thread function.
    Ho definito la prima bozza della funzione che gestira il thread per il socket UDP lato peer.

--- peer/peer_udp.c
+++ peer/peer_udp.c
@@ -3,10 +3,17 @@
 #include "../socket_utils.h"
 #include "../thread_semaphore.h"
 #include "../commons.h"
+#include "../unified_io.h"
 #include <signal.h>
+#include <sched.h>
 
 /* flag che verifica che il thread non sia già stato avviato */
 volatile int started;
+
+/** Id del thead che gestisce il socket
+ * UDP
+ */
+thread_t UDP_tid;
 
 /** Funzione che rappresenta il corpo del
  * thread che gestira il socket
@@ -14,15 +21,70 @@ volatile int started;
 static void* UDP(void* args)
 {
     struct thread_semaphore* ts;
+    int* data;
+    /* porta decisa dal padre */
+    int requestedPort;
+    /* porta usata dal thread */
+    int usedPort;
+    /* fd del socket */
+    int socket;
+    /* flag per evitare di sprecare
+     * troppi cicli di CPU cedendone
+     * il controllo al altri thread
+     * se per un ciclo non si trova
+     * a fare nulla */
+    int yield;
 
-    if (args == NULL)
-    {
+    ts = thread_semaphore_form_args(args);
+    if (ts == NULL)
         errExit("*** UDP ***\n");
+
+    data = (int*)thread_semaphore_get_args(args);
+    if (data == NULL)
+        errExit("*** UDP ***\n");
+
+    requestedPort = *data;
+
+    /* crea il socket UDP */
+    socket = initUDPSocket(requestedPort);
+    if (socket == -1)
+    {
+        if (thread_semaphore_signal(ts, -1, NULL) == -1)
+            errExit("*** UDP ***\n");
     }
+    /* verifica la porta */
+    usedPort = getSocketPort(socket);
+    if (usedPort == -1 || (requestedPort == 0 && requestedPort != usedPort))
+    {
+        if (thread_semaphore_signal(ts, -1, NULL) == -1)
+            errExit("*** UDP ***\n");
+    }
+    /* metodo per fornire al chiamante
+     * la porta utilizzata */
+    *data = usedPort;
+    /* socket creato, porta nota, si può iniziare */
+    if (thread_semaphore_signal(ts, 0, NULL) == -1)
+        errExit("*** UDP ***\n");
+
+    /* a questo punto il padre può riprendere */
+    unified_io_push("UDP thread running", UNIFIED_IO_NORMAL);
+    /* qui va il loop di gestione delle richieste */
+    yield = 0; /* il primo ciclo di sicuro non si lascia la CPU */
+    while (1)
+    {
+        if (yield)
+            sched_yield(); /* altruista */
+        else
+            yield = 1; /* forse la lascia al prossimo */
+
+        /* esamina le singole richiesta 1 per 1
+         * e se ne trova imposta yield=0 */
+
+    }
+    /* mai raggiunto */
 
     return NULL;
 }
-
 
 int UDPstart(int port)
 {
