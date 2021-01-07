0c23c484175cfb241c04638f0100d2445ac06ac5
Tredici <marcolucio1999@gmail.com>
2021-01-07 18:59:52 +0100

    Fixed Warning - in register_create.
    Introdotto un cast a (void*) per compatibilità tra puntatori differenti.

--- register.c
+++ register.c
@@ -294,7 +294,7 @@ struct e_register* register_create(struct e_register* r, int flag)
 
         return NULL;
     }
-    list_set_cleanup(ans->l, &register_free_entry);
+    list_set_cleanup(ans->l, (void*)&register_free_entry);
 
     return ans;
 }
