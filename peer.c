4afa48b89899d011e16283b2e4621598985917da
Tredici <marcolucio1999@gmail.com>
2021-01-07 16:05:18 +0100

    First version of peer main.
    Implementata la prima versione del main di un peer per iniziare a condurre i vari test di integrazione.

--- peer.c
+++ peer.c
@@ -3,13 +3,35 @@
  * di essere unito in un prodotto
  * definitivo
  */
+#include "peer/peer_stop.h"
 #include "repl.h"
+#include "main_loop.h"
+#include "unified_io.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "peer/peer_stop.h"
+#include <string.h>
+
+void usageHelp(const char* p)
+{
+    printf("%p <porta>\n");
+
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char* argv[])
 {
+    struct main_loop_command commands[] = {
+        { "stop", &stop, "termina il peer" }
+    };
+
+    int commandNumber = sizeof(commands)/sizeof(struct main_loop_command );
+
+    if (argc != 1 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+        usageHelp(argv[0]);
+
+
+    unified_io_init();
+    main_loop("INPUT", commands, commandNumber);
 
 
     printf("Peer terminato correttamente.\n");
