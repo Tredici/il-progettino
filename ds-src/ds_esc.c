fd5efb70707f8831917286c4d9165900f0d9e21d
Tredici <marcolucio1999@gmail.com>
2021-01-09 21:39:02 +0100

    Implemented (ds)esc.
    Implementato il gestore del comando di esc (che avvia la terminazione dei peer) del discovery server.

---
+++ ds-src/ds_esc.c
@@ -0,0 +1,8 @@
+#include "ds_esc.h"
+#include "../repl.h"
+
+int esc(const char* args)
+{
+    (void)args;
+    return OK_TERMINATE;
+}
---
+++ ds-src/ds_esc.h
@@ -0,0 +1,10 @@
+/** Comando di esc del main loop
+ * del discovery server
+ */
+
+#ifndef DS_ESC
+#define DS_ESC
+
+int esc(const char*);
+
+#endif
