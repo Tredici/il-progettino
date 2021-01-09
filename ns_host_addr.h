3fb83115024c435d5cc7b94cb4038e6c2e3469dc
Tredici <marcolucio1999@gmail.com>
2021-01-09 13:12:42 +0100

    Defined struct ns_host_addr.
    Definita una struttura per contenere in un formato facilmente scambiabile via rete in modo sicuro le informazioni per connettersi a un dato host.

--- ns_host_addr.h
+++ ns_host_addr.h
@@ -14,6 +14,27 @@
 
 #include <arpa/inet.h>
 
+/** Questa struttura raccoglie in
+ * un formato trasferibile in modo
+ * sicuro via rete le informazioni
+ * per contattare un dato host
+ */
+struct ns_host_addr
+{
+    uint8_t ipVersion; /* versione di ip - 4 o 6 */
+    uint16_t port; /* porta utilizzata - network order */
+    /* cosa che mi tocca fare per poter trasferire
+     * indirizzi sia ipV4 sia ipV6 */
+    union boot_req_ip_addr
+    {
+        /* questa cosa funziona solo perché
+         * queste informazioni sono di default
+         * in network order */
+        struct in_addr ipV4;
+        struct in6_addr ipV6;
+    } ip __attribute__ ((packed));
+};
+
 
 /** Due funzioni ausiliarie per scambiare in modo
  * sicuro via rete il formato degli indirizzi che
