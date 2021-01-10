b680851565a99e71c64208e4c5a7bf4150811af5
Tredici <marcolucio1999@gmail.com>
2021-01-10 16:59:44 +0100

    Improved (peer) start handler.
    Migliorata la funzione in vista dei test futuri.

--- peer-src/peer_start.c
+++ peer-src/peer_start.c
@@ -1,4 +1,5 @@
 #include "peer_start.h"
+#include "peer_udp.h"
 #include "../repl.h"
 #include <stdio.h>
 
@@ -14,6 +15,14 @@ int start(const char* args)
         return ERR_PARAMS;
     }
     printf("start: <%s> <%s>\n", hostname, portname);
+
+    if (UDPconnect(hostname, portname) == -1)
+    {
+        printf("Impossibile raggiungere [%s:%s]\n", hostname, portname);
+        return ERR_FAIL;
+    }
+
+    printf("Connessione al network riuscita!\n");
 
     return OK_CONTINUE;
 }
