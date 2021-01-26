082049b92323e0de4f3f653755d853ff36a4303e
Tredici <marcolucio1999@gmail.com>
2021-01-26 01:01:04 +0100

    Started developing of time_utils.
    Iniziato lo sviluppo di un insieme di funzioni ausiliarie per gestire dati e tempi.

---
+++ time_utils.c
@@ -0,0 +1,40 @@
+#include "time_utils.h"
+#include <errno.h>
+#include <string.h>
+
+int time_copy_date(struct tm* dst, const struct tm* src)
+{
+    if (dst == NULL || src == NULL)
+        return -1;
+
+    memset(dst, 0, sizeof(struct tm));
+    dst->tm_yday = src->tm_yday;
+    dst->tm_mon = src->tm_mon;
+    dst->tm_mday = src->tm_mday;
+
+    return 0;
+}
+
+int time_cmp(const struct tm* time1, const struct tm* time2)
+{
+    struct tm a, b;
+    time_t t_a, t_b;
+
+    if (time1 == NULL || time2 == NULL)
+    {
+        errno = EINVAL;
+        return 0;
+    }
+
+    time_copy_date(&a, time1);
+    time_copy_date(&b, time2);
+
+    t_a = mktime(&a); t_b = mktime(&b);
+
+    if (t_a < t_b)
+        return -1;
+    else if (t_a == t_b)
+        return 0;
+    else
+        return 1;
+}
---
+++ time_utils.h
@@ -0,0 +1,34 @@
+/** Funzioni ausiliarie per lavorare con le
+ * date e i tempi.
+ */
+
+#ifndef TIME_UTILS
+#define TIME_UTILS
+
+#include <time.h>
+
+/** Copia solo la data dalla prima
+ * struttura alla seconda.
+ *
+ * Restituisce 0 in caso di successo
+ * e -1 in caso di errore.
+ */
+int time_copy_date(struct tm*, const struct tm*);
+
+/** Confronta due oggetti di tipo
+ * struct tm facendo un confronto
+ * esclusivamente sulla data.
+ *
+ * Restituisce 0 se le due date
+ * corrispondono, un valore negativo
+ * se la prima è antecedente, un
+ * valore positivo se la seconda è
+ * antecedente.
+ * 
+ * In caso di errore restituisce 0
+ * e imposta errno a EINVAL
+ */
+int time_cmp(const struct tm*, const struct tm*);
+
+
+#endif
