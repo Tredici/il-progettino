1db52b421cbb5cb9e0c4835f0d243635c3184f30
Tredici <marcolucio1999@gmail.com>
2021-01-28 19:43:31 +0100

    Started development of (peer)get.
    Ho iniziato a sviluppare la funzione che si occuperà poi di svolgere i compiti richiesti dal comando get riconosciuto dai peer.

---
+++ peer-src/peer_get.c
@@ -0,0 +1,11 @@
+#include "peer_get.h"
+#include "../repl.h"
+
+int get(const char* args)
+{
+    char aggr[16];
+    char type[16];
+    char period[32];
+
+    return OK_CONTINUE;
+}
---
+++ peer-src/peer_get.h
@@ -0,0 +1,10 @@
+/** Questo file espone il gestiore del
+ * comando "get" riconosciuto dai peer.
+ */
+
+#ifndef PEER_GET
+#define PEER_GET
+
+int get(const char*);
+
+#endif
