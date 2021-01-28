6304dfb13b3cfae5024bd65b0268d6bdccf4b207
Tredici <marcolucio1999@gmail.com>
2021-01-28 15:55:57 +0100

    Improved TCP subsystem.
    Ho proseguito nell'estendere le caratteristiche del sottosistema TCP e adesso sono arrivato al punto in cui un nuovo thread viene effettivamente avviato e risulta funzionante.

--- peer-src/peer_tcp.c
+++ peer-src/peer_tcp.c
@@ -6,6 +6,23 @@
 #include "../messages.h"
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "../unified_io.h"
+#include "../commons.h"
+#include <signal.h>
+#include <fcntl.h>
+
+/** CONVENZIONE SUL COLLEGAMENTO TRA PEER:
+ * dovrà essere sempre il peer con ID più
+ * altro a connettersi ai vicini con ID
+ * più basso. "Chi prima arriva meno fatica."
+ */
+
+/** Segnale che sarà utilizzato per
+ * richiedere al sottosistema di terminare.
+ * È volutamente lo stesso impiegato per
+ * il sottosistema ENTRIES.
+ */
+#define TERM_SUBSYS_SIGNAL SIGQUIT
 
 /** Parametro da usare per la listen
  * sul socket TCP
@@ -16,6 +33,10 @@
  * il thread TCP è già stato avviato.
  */
 static int activated;
+
+/** Thread id del thread TCP
+ */
+static pthread_t TCP_tid;
 
 /** Descrittore di file
  * del socket tcp
@@ -39,15 +60,109 @@ int TCPinit(int port)
     if (sk == -1)
         return -1;
 
+    /* lo rende non bloccante */
+    if (fcntl(sk, F_SETFL, O_NONBLOCK) != 0)
+    {
+        close(sk);
+        return -1;
+    }
+
     tcpFd = sk;
     activated = 1;
 
     return 0;
 }
 
-int TCPrun(uint32_t ID, struct peer_data* neighbours, size_t N)
+/** Struttura dati atta a contenere
+ * le informazioni e il socket
+ * per raggiungere un peer
+ */
+struct peer_tcp
 {
-    running;
+    /* informazioni sul vicino */
+    struct peer_data data;
+    /* socket per raggiungerlo */
+    int sockfd;
+};
+
+/** Cerca di instaurare una connessione
+ * TCP con il peer le cui informazioni
+ * sono fornite nell'oggetto peer_data.
+ *
+ * ATTENZIONE: non effettua alcun controllo
+ * sulla relazione d'ordine
+ */
+static int connectToPeer(const struct peer_data* data)
+{
+    int sk; /* descrittore del socket */
+
+    if (data == NULL)
+        return -1;
+    sk = connect_to_ns_host_addr(&data->ns_addr);
+
+    return sk;
+}
+
+/** Variabili che permetto al thread di
+ * ottenere le informazioni sui suoi
+ * vicini */
+static uint32_t peerID; /* ID nel netword */
+static struct peer_data neighbours[MAX_TCP_CONNECTION]; /* dati sui vicini */
+static size_t neighboursNumber; /* numero di vicini. */
+
+/** Codice del thread TCP. Sarà attivato al
+ * momento della connessione al network.
+ */
+static void* TCP(void* args)
+{
+    struct thread_semaphore* ts;
+    char buffer[128];
+    int i;
+
+    ts = thread_semaphore_form_args(args);
+    if (ts == NULL)
+        errExit("*** TCP ***\n");
+
+    unified_io_push(UNIFIED_IO_NORMAL, "Starting thread TCP...");
+    unified_io_push(UNIFIED_IO_NORMAL, "Peer ID (%ld)", (long)peerID);
+    unified_io_push(UNIFIED_IO_NORMAL, "Num. Neighbours (%ld)", (long)neighboursNumber);
+    for (i = 0; i != (int)neighboursNumber; ++i)
+    {
+        peer_data_as_string(&neighbours[i], buffer, sizeof(buffer));
+        unified_io_push(UNIFIED_IO_NORMAL, "%d)-> %s", i, buffer);
+    }
+
+    /* Ora è pronto */
+    if (thread_semaphore_signal(ts, 0, NULL) == -1)
+        errExit("*** UDP ***\n");
+
+    unified_io_push(UNIFIED_IO_NORMAL, "Thread TCP running.");
+
+    return NULL;
+}
+
+
+int TCPrun(uint32_t ID, struct peer_data* initNeighbours, size_t N)
+{
+    int i;
+
+    if (running)
+        return -1;
+
+    if (N > MAX_NEIGHBOUR_NUMBER)
+        errExit("*** TCPrun(N > MAX_NEIGHBOUR_NUMBER) ***\n");
+
+    /* rende i dati disponibili al nuovo thread */
+    peerID = ID; /* l'ID del peer */
+    for (i = 0; i != (int)N; ++i) /* i dati per raggiungere i vicini */
+        neighbours[i] = initNeighbours[i];
+
+    neighboursNumber = N; /* il numero iniziale di vicini */
+
+    if (start_long_life_thread(&TCP_tid, &TCP, NULL, NULL) == -1)
+        return -1;
+
+    running = 1;
     return 0;
 }
 
@@ -60,6 +175,10 @@ int TCPclose(void)
     if (running)
     {
         /* abbiamo un thread da fermare */
+
+        /* esegue una join */
+        if (pthread_join(TCP_tid, NULL) != 0)
+            errExit("*** TCPclose:pthread_join ***\n");
         running = 0;
     }
 
