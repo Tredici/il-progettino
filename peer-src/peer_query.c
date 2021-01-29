de246a4d0362997ca2e2916a57b4af58c1151d6c
Tredici <marcolucio1999@gmail.com>
2021-01-30 00:26:10 +0100

    Started develo of (peer)query utils.
    Ho iniziato a sviluppare le funzioni che userò per gestire le query e i loro risultati.

---
+++ peer-src/peer_query.c
@@ -0,0 +1,1 @@
+#include "peer_query.h"
---
+++ peer-src/peer_query.h
@@ -0,0 +1,14 @@
+/** Raccolta di funzioni e strutture per contenere
+ * i risultati delle query.
+ */
+
+#ifndef PEER_QUERY
+#define PEER_QUERY
+
+#include "../register.h"
+#include "../list.h"
+
+
+
+
+#endif
